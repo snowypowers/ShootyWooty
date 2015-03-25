7df7f9e626fa4b8a2cf2124666fe7cb39a9c891f
anvithap <anvithaprashanth@yahoo.co.in>
2015-03-26 01:11:18 +0800

    Created room and logged in players

--- Shooty(JX)/Shooty/core/src/com/eye7/ShootyWooty/helper/ActionResolver.java
+++ Shooty(JX)/Shooty/core/src/com/eye7/ShootyWooty/helper/ActionResolver.java
@@ -4,8 +4,8 @@ package com.eye7.ShootyWooty.helper;
  * Created by anvithaprashanth on 17/03/15.
  */
 public interface ActionResolver {
-    public boolean getSignedInGPGS();
-    public void loginGPGS();
+//    public boolean getSignedInGPGS();
+//    public void loginGPGS();
 //    public void submitScoreGPGS(int score,String id);
 //    public void unlockAchievementGPGS(String achievementId);
 //    public void getLeaderboardGPGS(String id);
--- Shooty(JX)/Shooty/core/src/com/eye7/ShootyWooty/main.java
+++ Shooty(JX)/Shooty/core/src/com/eye7/ShootyWooty/main.java
@@ -11,16 +11,6 @@ import com.eye7.ShootyWooty.helper.MainLoader;
 import com.eye7.ShootyWooty.screen.MainScreen;
 
 
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.eye7.ShootyWooty.helper.ActionResolver;
-import com.eye7.ShootyWooty.helper.MainLoader;
-import com.eye7.ShootyWooty.screen.MainScreen;
-
-
 public class main extends Game {
     SpriteBatch batch;
     Texture img;
@@ -41,7 +31,6 @@ public class main extends Game {
         scaleY = Gdx.graphics.getHeight() / 540.0f;
         MainLoader.load();
         setScreen(new MainScreen(this));
-        actionResolver.loginGPGS();
         Gdx.app.log("Screen Reso:", "Width: " + String.valueOf(Gdx.graphics.getWidth()));
         Gdx.app.log("Screen Reso:", "Height: " + String.valueOf(Gdx.graphics.getHeight()));
         Gdx.app.log("ScaleX", String.valueOf(scaleX));
